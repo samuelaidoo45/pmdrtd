f88293b56bba5f67b3ef708a2b30f8391ddef156
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2023-05-14 23:01:10 +0000

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Home from "./pages/home/Home";
 import LandingPage from "./pages/landingpage/LandingPage";
 import Login from "./pages/login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useContext } from "react";
 
 function App() {
 
