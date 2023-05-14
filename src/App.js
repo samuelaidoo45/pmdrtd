5d2553b03e1feae70d7d6234f73fb0d34eadce82
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2023-05-14 22:43:22 +0000

    update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
             <Route index element={<LandingPage />} />
             <Route path="login" element={<Login />} />
             <Route path="dashboard" element={<Home />} />
-
           </Route>
         </Routes>
       </BrowserRouter>
