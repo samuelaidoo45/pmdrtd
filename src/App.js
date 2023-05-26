62180f793ba7d8edb84f662de86df85b860d7150
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2023-05-26 12:15:26 +0000

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="app">
-      <BrowserRouter basename="/pomodoropal/build">
+      <BrowserRouter basename="build">
         <Routes>
           <Route path="/">
             <Route index element={<LandingPage />} />
