8cbf09f6309178efbf7f73291e9272239eb4132f
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2023-06-10 17:34:02 +0000

    update

--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
-// const userRoutes = require('./routes/userRoutes');
-// const toDoItemRoutes = require('./routes/toDoItemRoutes');
-// const cors = require('cors');
-// const db = require('../config/database');
+const userRoutes = require('./routes/userRoutes');
+const toDoItemRoutes = require('./routes/toDoItemRoutes');
+const cors = require('cors');
+const db = require('../config/database');
 
-// const session = require('express-session');
-// const crypto = require('crypto');
-// //const path = require('path');
+const session = require('express-session');
+const crypto = require('crypto');
+//const path = require('path');
 
-// const secret = crypto.randomBytes(64).toString('hex');
+const secret = crypto.randomBytes(64).toString('hex');
 
 
 const app = express();
@@ -17,34 +17,34 @@ app.get('/pomodoropal/api/hello',()=>{
   console.log("Hello World");
 });
 
-// db.authenticate()
-//   .then(() => {
-//     console.log('Database connected');
-//     return db.sync(); // Sync models with the database
-//   })
-//   .then(() => {
-//     console.log('Models synced');
-//   })
-//   .catch((err) => {
-//     console.error('Error connecting to the database:', err);
-// });
+db.authenticate()
+  .then(() => {
+    console.log('Database connected');
+    return db.sync(); // Sync models with the database
+  })
+  .then(() => {
+    console.log('Models synced');
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database:', err);
+});
 
 
 //middlewares
-// app.use(cors());
-// app.use(express.json());
-// app.use(
-//   session({
-//     secret: secret,
-//     resave: false,
-//     saveUninitialized: false,
-//   })
-// );
+app.use(cors());
+app.use(express.json());
+app.use(
+  session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 
 
-// app.use('/pomodoropal/api/user', userRoutes);
-// app.use('/pomodoropal/api/todo',toDoItemRoutes);
+app.use('/pomodoropal/api/user', userRoutes);
+app.use('/pomodoropal/api/todo',toDoItemRoutes);
 
 
 const port = 5000; // Replace with your desired port number
