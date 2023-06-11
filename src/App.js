d9e6b79edefc6da4fb5ec95062199640209bc046
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2023-06-11 13:21:16 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Home from "./pages/home/Home";
 import LandingPage from "./pages/landingpage/LandingPage";
 import Login from "./pages/login/Login";
-import { BrowserRouter, StaticRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter,  Route } from 'react-router-dom';
 import React from 'react';
 
 
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="app">
-      <StaticRouter basename="/pomodoropal/build">
+      <BrowserRouter basename="/pomodoropal/build">
         <Routes>
           <Route path="/">
             <Route index element={<LandingPage />} />
@@ -18,7 +18,7 @@ function App() {
 
           </Route>
         </Routes>
-      </StaticRouter>
+      </BrowserRouter>
     </div>
   );
 }
