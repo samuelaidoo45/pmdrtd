c28771e34b319a1750123cfa86162c0ac4725f0a
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2023-06-11 00:36:15 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,48 +1,26 @@
-// import Home from "./pages/home/Home";
-// import LandingPage from "./pages/landingpage/LandingPage";
-// import Login from "./pages/login/Login";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import React from 'react';
-
-
-// function App() {
-
-//   return (
-//     <div className="app">
-//       <BrowserRouter basename="/pomodoropal/build">
-//         <Routes>
-//           <Route path="/">
-//             <Route index element={<LandingPage />} />
-//             <Route path="login" element={<Login />} />
-//             <Route path="dashboard" element={<Home />} />
-
-//           </Route>
-//         </Routes>
-//       </BrowserRouter>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React from 'react';
+import Home from "./pages/home/Home";
+import LandingPage from "./pages/landingpage/LandingPage";
+import Login from "./pages/login/Login";
 import { BrowserRouter, StaticRouter, Route, Switch } from 'react-router-dom';
-import Home from './pages/home/Home';
-import LandingPage from './pages/landingpage/LandingPage';
-import Login from './pages/login/Login';
+import React from 'react';
 
-function App({ isServer }) {
-  const Router = isServer ? StaticRouter : BrowserRouter;
+
+function App() {
+
+  const Router = true ? StaticRouter : BrowserRouter;
 
   return (
     <div className="app">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/login" component={Login} />
-          <Route path="/dashboard" component={Home} />
-        </Switch>
-      </Router>
+      <BrowserRouter basename="/pomodoropal/build">
+        <Routes>
+          <Route path="/">
+            <Route index element={<LandingPage />} />
+            <Route path="login" element={<Login />} />
+            <Route path="dashboard" element={<Home />} />
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
