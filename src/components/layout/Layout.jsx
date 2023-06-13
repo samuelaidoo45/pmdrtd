2ee1d98f1e97baebd65ca8186ea763a025f6dec6
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2023-06-13 01:11:35 +0000

    update

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import MainStyle from './Layout.module.css';
 import { Link } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 
 
 const Layout = ({ children }) => {
