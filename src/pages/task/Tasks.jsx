7abc8708c0185171b3e84f76a5c26b4989d53a0e
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2023-06-13 01:55:36 +0000

    update

--- src/pages/task/Tasks.jsx
+++ src/pages/task/Tasks.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 // import Dashboard from "../../components/home/Home";
 import Layout from "../../components/layout/Layout";
 
-import "./home.css";
 
 const Tasks = () => {
   return (
--- src/pages/timer/Timer.jsx
+++ src/pages/timer/Timer.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 // import Dashboard from "../../components/home/Home";
 import Layout from "../../components/layout/Layout";
 
-import "./home.css";
-
 const Timer = () => {
   return (
     <div className="home">
