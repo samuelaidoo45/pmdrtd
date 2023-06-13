172c2eda0413fdfc3185ec1d52a7847f8d915d1e
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2023-06-13 00:22:33 +0000

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Main from "../../components/main/Main";
-// import Layout from "../../components/layout/Layout";
+import Layout from "../../components/layout/Layout";
 import "./home.css";
 
 const Home = () => {
