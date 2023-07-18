62138fc4faaaa9caaaade79a0e7cce29683fb7de
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2023-07-18 01:05:02 +0000

    update

--- api/src/controllers/UserController.js
+++ api/src/controllers/UserController.js
@@ -22,7 +22,7 @@ async function login(req, res) {
     return res.status(200).json({ message: 'Login successful' });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: 'Server error' });
+    return res.status(500).json({ message: error });
   }
 }
 
